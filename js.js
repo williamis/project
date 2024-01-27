9289430ffcba73a2266781b4669135fee31727ca
williamis <vilqst.og@gmail.com>
2024-01-27 16:47:52 +0200

    javascrip alotettu

--- js.js
+++ js.js
@@ -1,1 +1,21 @@
-// koodia
+function addTodo() {
+    const todoInput = document.getElementById('todoInput');
+    const todoList = document.getElementById('todoList');
+    
+    if (todoInput.value.trim() !== "") {
+      const newTodo = document.createElement('li');
+      newTodo.textContent = todoInput.value;
+      
+      // delete nappi tähä jos haluut
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = 'Poista';
+      deleteButton.onclick = function() {
+        todoList.removeChild(newTodo);
+      };
+      
+      newTodo.appendChild(deleteButton);
+      
+      todoList.appendChild(newTodo);
+      todoInput.value = "";
+    }
+  }
