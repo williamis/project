b13f017f83dbdb4d9e608e543d98845adb44b896
williamis <vilqst.og@gmail.com>
2024-01-27 16:52:26 +0200

    css & js lisättu

--- js.js
+++ js.js
@@ -19,3 +19,34 @@ function addTodo() {
       todoInput.value = "";
     }
   }
+
+  // lskin & kello
+
+
+let display = document.getElementById('display');
+let memo = document.getElementById('memo');
+
+function appendToDisplay(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = '';
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = 'Error';
+  }
+}
+
+
+function clearCalculator() {
+    display.value = '';
+  }
+  
+  function clearMemo() {
+    memo.value = '';
+  }
